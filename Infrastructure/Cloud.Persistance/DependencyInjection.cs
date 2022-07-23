c4b356e3540216edcdb7df7628b1d8797fb12db1
ivk <ivan.keynes@bbconsult.co.uk>
2022-07-23 17:10:03 +0700

    Added dependency injection

---
+++ Infrastructure/Cloud.Persistance/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Cloud.Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cloud.Persistance
+{
+   static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistance (this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration["DbConnection"];
+            services.AddDbContext<CloudDbContext>(options =>
+            {
+                options.UseSqlite(connectionString);
+            });
+            services.AddScoped<ICloudDbContext>(provider => provider.GetService<CloudDbContext>());
+            return services;
+        }
+    }
+}
